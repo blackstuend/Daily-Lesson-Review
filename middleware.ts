aca1efbc3a5cd182ee4809d3cc48361302d3545c
Lucian <lucian.l@qycocu.com>
2025-10-27 16:45:38 +0800

    Refactor middleware configuration to restrict matcher to authentication paths and remove unnecessary logging.

--- middleware.ts
+++ middleware.ts
@@ -2,10 +2,9 @@ import { updateSession } from "@/lib/supabase/middleware"
 import type { NextRequest } from "next/server"
 
 export async function middleware(request: NextRequest) {
-  console.log('trigger middleware');
   return await updateSession(request)
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)"],
+  matcher: ["/auth/:path*"],
 }
